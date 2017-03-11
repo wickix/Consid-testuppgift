4e1a01da043352969a5d6166d66e7b0dd3384c39
wickix <wictooria@gmail.com>
2017-03-11 17:01:13 +0100

    Created

---
+++ Consid/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Consid
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Consid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Consid.MvcApplication" Language="C#" %>
